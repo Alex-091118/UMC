71f2dbb842d50848898ddedbe99871224fe7f0a4
a.koznev <aleksei.koznev@stm-labs.ru>
2025-07-29 13:39:33 +0300

    fixed n+1 queries for events query

--- dao-postgres/src/main/java/ru/mts/media/platform/umc/dao/postgres/event/EventPgRepository.java
+++ dao-postgres/src/main/java/ru/mts/media/platform/umc/dao/postgres/event/EventPgRepository.java
@@ -1,6 +1,13 @@
 package ru.mts.media.platform.umc.dao.postgres.event;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EventPgRepository extends JpaRepository<EventPgEntity, String> {
+
+    @EntityGraph(attributePaths = {"venues"})
+    List<EventPgEntity> findAll();
+
 }
